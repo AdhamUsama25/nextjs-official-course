e5642e6dcbed373af6bcf480d8085a4af1cf4367
Adham Usama <adhamusama25@gmail.com>
2024-09-26 12:33:06 +0300

    fix import next/image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import { lusitana } from "./ui/fonts";
 
 export default function Page() {
